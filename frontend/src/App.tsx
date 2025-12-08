6e34429d7f1af2a2e540c53505fb1a0abce14530
Omer Faruk Cinoglu <62906173+omerfcinoglu@users.noreply.github.com>
2025-12-08 21:58:41 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css'
 import { Dashboard } from './components/dashboard'
+import { AnalysisTable } from './components/AnalysisHistory/analysisTable'
 
 function App() {
 
   return (
     <div>
-      <h1>Welcome to Dashboard</h1>
-      <Dashboard />
+      <div className="flex flex-col items-center justify-center">
+        <p className='text-6xl p-2'>Welcome to Dashboard</p>
+        <Dashboard />
+      </div>
+      <div className='flex flex-col'>
+        {/* <p className='text-2xl p-2 text-left'>Previous Analysis</p> */}
+        {/* <AnalysisTable /> */}
+      </div>
     </div>
   )
 }
