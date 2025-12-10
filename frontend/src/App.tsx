6a3618dfaf56ed7626a9d1f561436d7522e021f3
Omer Faruk Cinoglu <62906173+omerfcinoglu@users.noreply.github.com>
2025-12-10 22:20:17 +0300

    Backend init tested

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,12 @@
 import './App.css'
-import { Dashboard } from './components/dashboard'
-import { AnalysisTable } from './components/AnalysisHistory/analysisTable'
+import { TestBackend } from './components/TestBackend'
 
 function App() {
-
   return (
     <div>
       <div className="flex flex-col items-center justify-center">
         <p className='text-6xl p-2'>Welcome to Dashboard</p>
-        <Dashboard />
-      </div>
-      <div className='flex flex-col'>
-        {/* <p className='text-2xl p-2 text-left'>Previous Analysis</p> */}
-        {/* <AnalysisTable /> */}
+        <TestBackend />
       </div>
     </div>
   )
